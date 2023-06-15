a0fbcebd7f8fbfe14324db428c35c6c3065a52b1
valentin-d-r <valentin.dremaux@viacesi.fr>
2023-06-15 10:56:46 +0200

    Router Fonctionnel

--- easyservice/src/App.js
+++ easyservice/src/App.js
@@ -1,50 +1,24 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch,
-  Routes, 
-} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './front/Home';
+import Profile from './front/Profile';
+import MesDemandes from './front/Demandes';
+import NotFound from './front/Erreur404';
 
-import './App.css';
+function App() {
+  return (
+    <body>
+      <header></header>
 
-import Home from './front/Home.js';
-import Profile from './front/Profile.js';
-import MesDemandes from './front/Demandes.js';
-import NotFound from './front/Erreur404.js';
-
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <header>
-            <section>
-              <h1> react-starter </h1>
-              <h2> (React version 18.0.2) </h2>
-            </section>
-            <nav>
-              <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/front/Profile.js">Profile</Link></li>
-                <li><Link to="/front/Demandes.js">MesDemandes</Link></li>
-              </ul>
-            </nav>
-          </header>
-          <main>
-            <Routes>
-              <Route exact path="/" component={Home} />
-              <Route path="/front/Profile.js" component={Profile} />
-              <Route path="/front/Demandes.js" component={MesDemandes} />
-              <Route exact path="*" component={NotFound} />
-            </Routes>
-          </main>
-        </div>
-      </Router >
-    );
-  }
+      <div className="EasyService">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Demandes" element={<MesDemandes />} />
+          <Route path="/Erreur404" element={<NotFound />} />
+        </Routes>
+      </div>
+    </body>
+  );
 }
 
 export default App;
