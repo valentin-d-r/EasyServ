3b8d4cad53a201033d6b2589093d94043613f316
Valentin <valentin.dremaux@viacesi.fr>
2023-06-26 10:51:34 +0200

    Register bientôt fini

--- easyservice/src/front/Register.js
+++ easyservice/src/front/Register.js
@@ -1,29 +1,66 @@
 import { Link } from "react-router-dom";
 import React, {useState} from "react";
 import "../style/Register.css";
+import axios from "axios";
 
-export default function InscriptionPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [nom, setNom] = useState('');
-  const [prenom, setPrenom] = useState('');
-
-
-
-//Faire l'envoie de données d'enregistrement dans le front avec les variables email, password, nom et prenom
+const handleSubmit = async (e) => {
+  console.log("----|||||test");
+  e.preventDefault();
   
-return (
+  // Récupérer les valeurs des champs du formulaire
+  const prenom = e.target.elements.Name.value;
+  const nom = e.target.elements.LastName.value;
+  const email = e.target.elements.Email.value;
+  const password = e.target.elements.Password.value;
+  // Ajoutez d'autres champs selon vos besoins
+
+  // Créez un objet contenant les données à envoyer
+  const data = {
+    nom,
+    prenom,
+    email,
+    password,
+    // Ajoutez d'autres champs selon vos besoins
+  };
+
+  try {
+    // Effectuer la requête POST
+    console.log("----|||||test");
+    const response = await axios.post('localhost:3000/', data);
+
+    // Traitez la réponse si nécessaire
+    console.log("----|||||test");
+    console.log(response.data);
+  } catch (error) {
+    // Gérez les erreurs de requête
+    console.error(error);
+  }
+};
+  
+const Register = () => (
     <div className="reg-container">
-      <form className="form " method="post" action="" id="createAccount">
+      <form className="form " onSubmit={handleSubmit} id="createAccount">
         <h1 className="form__title">Créer un compte</h1>
         <div className="form__message form__message--error" />
         <div className="form__input-group">
           <input
             type="text"
-            id="Username"
+            id="Name"
+            name="Name"
             className="form__input"
             autofocus
-            placeholder="Nom de compte"
+            placeholder="Prénom"
+          />
+          <div className="form__input-error-message" />
+        </div>
+        <div className="form__input-group">
+          <input
+            type="text"
+            id="LastName"
+            name="LastName"
+            className="form__input"
+            autofocus
+            placeholder="Nom"
           />
           <div className="form__input-error-message" />
         </div>
@@ -31,10 +68,10 @@ return (
           <input
             type="email"
             id="Email"
+            name="Email"
             className="form__input"
             autofocus
             placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)} pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$' required
           />
           <div className="form__input-error-message" />
         </div>
@@ -42,10 +79,10 @@ return (
           <input
             type="Mot de passe" 
             id="Password"
+            name="Password"
             className="form__input"
             autofocus
             placeholder="Mot de passe"
-            onChange={(e) => setPassword(e.target.value)} required
           />
           <div className="form__input-error-message" />
         </div>
@@ -72,4 +109,4 @@ return (
       </form>
     </div>
   )
-}
+  export default Register
