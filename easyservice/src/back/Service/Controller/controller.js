c87fb12e1414b2c5bf3d7ccc5f05dfa481748e20
ryaaan <96530662+ayrnn599@users.noreply.github.com>
2023-06-27 20:21:29 +0200

    ajouts fonctionnalités service et token

--- easyservice/src/back/Service/Controller/controller.js
+++ easyservice/src/back/Service/Controller/controller.js
@@ -1,23 +1,14 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 const schema = require('../Models/models');
 
 exports.addService = (req, res, next) => {
 
-    // clientId: { type: int, required: true },
-    // titre: {type: String, required: true},
-    // description: {type: String, required: true},
-    // status: { type: String, enum: ['en attente', 'acceptée', 'en cours', 'terminée'], default: 'en attente' },
-    // prestataireId: { type: Schema.Types.ObjectId, ref: 'Prestataire'},
-    // dateCreation: {
-    //     type: Date, default: Date.now },
-    // dateModification: { type: Date }
     serviceData = {
         clientId: req.body.clientId,
         titre: req.body.titre,
         description: req.body.description
-        
-
     };
 
     console.log("---> Service");
@@ -38,10 +29,116 @@ exports.addService = (req, res, next) => {
 
 
 exports.modifyService = (req, res, next) => { 
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1]; //psk c'est un bearer
+
+    if (!token) {
+        return res.sendStatus(401).json(error); 
+    }
+
+    jwt.verify(token, 'chiffrement', (err, decoded) => {
+        if (err) {
+            return res.sendStatus(403).json(err); 
+        }
+        
+        const idService = decoded.idService;
+        console.log("--> Idservice");
+        console.log(idService);
+        
+        const update = req.body;
+        console.log("---> Update");
+        console.log(update);
+       
+
+        schema.updateOne({ _id: idService }, update)
+            .then(result => {
+                res.json({ message: 'Document mis à jour', result });
+            })
+            .catch(err => {
+                res.status(500).json({ err });
+            });
+
+
+        
+    });
+
 
 };
 
-exports.showServices = (req, res, next) => {
+exports.showAllServices = (req, res, next) => {
+    schema.find()
+    .then((results) => {
+        console.log(results);
+        res.status(200).json({results});
+    })
+    .catch((error) => {
+        console.log(error);
+        res.status(400).json({message: error});
+    });
+
+};
+
+exports.deleteService = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1]; //psk c'est un bearer
+
+    if (!token) {
+        return res.sendStatus(401).json(error); 
+    }
+
+    jwt.verify(token, 'chiffrement', (err, decoded) => {
+        if (err) {
+            return res.sendStatus(403).json(err); 
+        }
+        
+        const idService = decoded.idService;
+        console.log("--> Idservice");
+        console.log(idService);
+        
+        const update = req.body;
+        console.log("---> Update");
+        console.log(update);
+       
+
+        schema.deleteOne({ _id: idService })
+            .then(result => {
+                res.json({ message: 'Document supprimé', result});
+            })
+            .catch(err => {
+                res.status(500).json({ err });
+            });
+
+
+        
+    });
+
+};
+
+exports.showToken = (req, res, next) => {
+    console.log("--->Token");
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1]; //psk c'est un bearer 
+    console.log(token);
+
+    if (!token) {
+        return res.sendStatus(401).json(error); 
+    }
+
+    jwt.verify(token, 'chiffrement', (err, decoded) => {
+        if (err) {
+            return res.sendStatus(403).json(err); 
+        }
+
+        const userId = decoded.id;
+        const role = decoded.role;
+
+        console.log("--->Id");
+        console.log(userId);
+        console.log(role);
+        res.status(200).json({message: "Requete reussi"});
+    });
+
+    console.log('Fin de code');
 
 
 };
--- easyservice/src/back/Service/Routes/routes.js
+++ easyservice/src/back/Service/Routes/routes.js
@@ -4,6 +4,9 @@ const serviceCtrl = require('../Controller/controller')
 
 router.post('/addService', serviceCtrl.addService);
 router.patch('/updateService', serviceCtrl.modifyService);
+router.get('/showAllService', serviceCtrl.showAllServices);
+router.get('/tokentest', serviceCtrl.showToken);
+router.delete('/deleteService', serviceCtrl.deleteService)
 
 
 module.exports = router;
