749341301d0f9ffba0f305d58fd29c10095848b4
amauroy <94054434+amauroy@users.noreply.github.com>
2023-06-28 16:14:37 +0200

    Erreur 404 page

--- easyservice/src/front/Erreur404.js
+++ easyservice/src/front/Erreur404.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import '../style/Error404.css';
+import Logo from "../LogoES.png"
+import { Link } from 'react-router-dom';
+import home from "./Home"
 
-const NotFound = () => (
-  <p>
-  NotFound works!
-</p>
-)
+const Error404 = () => {
+  return (
+    <div className="error-container">
+      <img src={Logo} alt="Logo de la marque" className="logo" />
+      <h1 className="title">Erreur 404</h1>
+      <div className="description">La page que vous recherchez est introuvable.</div>
+      <Link className="back-button" to="/" components={home} id="linkHome">
+        Retourner à l'accueil
+      </Link>
 
-export default NotFound
+
+
+    </div>
+  );
+};
+
+export default Error404;
