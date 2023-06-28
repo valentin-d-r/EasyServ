d846796b80d17628356bbd0467357f1ddcfd7929
amauroy <94054434+amauroy@users.noreply.github.com>
2023-06-28 14:47:01 +0200

    update profil

--- easyservice/src/front/Profil.js
+++ easyservice/src/front/Profil.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../style/Profil.css';
-import chat from "../LogoChat.png"
+import chat from "../LogoChat.png";
+import photo from "../photo.png";
 const Profil = () => {
   const handleChatClick = () => {
     // Rediriger vers la page de chat
@@ -21,11 +22,11 @@ const Profil = () => {
     <div className="profil-container">
       <div className="profil-details">
         <div className="profil-picture">
-          <img src="" alt="Photo de profil" />
+          <img src={photo} alt="Photo de profil" />
         </div>
         <div className="profil-info">
-          <h2>Nom Prénom</h2>
-          <p>nom.prenom@mail.com</p>
+          <h2>Roy Amaury</h2>
+          <p>Amaury.roy@viacesi.fr</p>
         </div>
       </div>
       <div className="profil-actions">
