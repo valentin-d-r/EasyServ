eb0c0f6301e56e257a2743f48770662ec9cfd1df
amauroy <94054434+amauroy@users.noreply.github.com>
2023-06-29 13:50:53 +0200

    Offer_job update

--- easyservice/src/Components/OfferJob.js
+++ easyservice/src/Components/OfferJob.js
@@ -2,9 +2,13 @@ import '../style/OfferJob.css'
 
 const Offer = () => (
     <div className="offer-wrapper">
+
         <div className="offer_title">Titre de l'annonce</div>
         <div className="offer_description">Description de l'annonce</div>
-        <input type="button" className="voir_offre" Value="Voir l'offre" />
+        <div className="offer_button">
+            <input type="button" className="accepter_offre" Value="Accepter" />
+            <input type="button" className="refuser_offre" value="Refuser" />
+        </div>
     </div>
 
 )
